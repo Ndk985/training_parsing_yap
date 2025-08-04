594d71c6c09f5ec7ebc6fa28007dec10cc7af3a6
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-08-04 12:04:16 +0300

    алхимия CRUD

--- work_with_db.py
+++ work_with_db.py
@@ -1,20 +1,42 @@
 # work_with_db.py
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Session, declared_attr, declarative_base
+from sqlalchemy import insert
+from sqlalchemy import select
+from sqlalchemy import update
+from sqlalchemy import delete
 
-Base = declarative_base()
+
+# Обычно класс, на основе которого создаётся декларативная база,
+# называют так же, как и сам класс декларативной базы.
+class Base:
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=Base)
 
 
 class Pep(Base):
-    __tablename__ = 'pep'  # Задали имя таблицы в БД.
-
-    # Описываем свойства модели/колонки таблицы:
-    id = Column(Integer, primary_key=True)
     pep_number = Column(Integer, unique=True)
     name = Column(String(200))
     status = Column(String(20))
 
+    def __repr__(self):
+        # При представлении объекта класса Pep 
+        # будут выводиться значения полей pep_number и name.
+        return f'PEP {self.pep_number} {self.name}'
+
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///sqlite.db', echo=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('sqlite:///sqlite.db', echo=False)
+    session = Session(engine)
+
+    session.execute(
+    delete(Pep).where(Pep.status == 'Active')
+    )
+    session.commit()
